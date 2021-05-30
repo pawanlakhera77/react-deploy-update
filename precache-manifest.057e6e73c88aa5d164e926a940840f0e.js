2580ca885963ed1eb4b9172607333e840aae1f21
Pawan lakhera <pawanlakhera77@gmail.com>
2021-05-31 04:16:58 +0530

    Updates

---
+++ precache-manifest.057e6e73c88aa5d164e926a940840f0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b879d0c77c8cdf68ba16f88e886c435",
+    "url": "/cra-sw/index.html"
+  },
+  {
+    "revision": "5b07b9b7e0df03e6ea55",
+    "url": "/cra-sw/static/css/main.837db68a.chunk.css"
+  },
+  {
+    "revision": "a5d0ff6c478aa5b433aa",
+    "url": "/cra-sw/static/js/2.755c874a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/cra-sw/static/js/2.755c874a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b07b9b7e0df03e6ea55",
+    "url": "/cra-sw/static/js/main.43ee2c9a.chunk.js"
+  },
+  {
+    "revision": "0cc9977b542b6bae7c13",
+    "url": "/cra-sw/static/js/runtime-main.46f53687.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/cra-sw/static/media/logo.ee7cd8ed.svg"
+  }
+]);
